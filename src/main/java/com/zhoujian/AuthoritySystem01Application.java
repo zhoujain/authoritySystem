ed63419656b2204d61504e00bc6ce99d8cd21081
zhoujian <zhoujian0727@foxmail.com>
2019-08-13 14:29:24 +0800

    新建项目

---
+++ src/main/java/com/zhoujian/AuthoritySystem01Application.java
@@ -0,0 +1,13 @@
+package com.zhoujian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthoritySystem01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthoritySystem01Application.class, args);
+    }
+
+}
